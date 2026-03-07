5d99733238a9f55489212b9a5815bc89c8083b17
Vitor <vitor2017go@gmail.com>
2026-03-07 20:54:19 -0300

    Corrigindo IA do whatsapp. V2.8

--- src/app/api/webhook/whatsapp/route.ts
+++ src/app/api/webhook/whatsapp/route.ts
@@ -117,7 +117,7 @@ export async function POST(req: Request) {
 
         const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); // Usando o modelo mais rápido
+        const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" }); // Usando o modelo mais rápido
 
         const body = await req.json();
 
